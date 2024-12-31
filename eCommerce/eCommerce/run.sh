8cc9402febad5dcd911cef310bba8bd63371dcce
Israel Obando <israel.obando@koerber-supplychain.com>
2024-12-31 09:57:53 +0100

    Deploy configs

--- eCommerce/eCommerce/run.sh
+++ eCommerce/eCommerce/run.sh
@@ -1,7 +1,4 @@
-#!/bin/bash
-echo "Building application with Maven..."
-mvn clean package
-echo "Building Docker image..."
+echo Building Docker image...
 docker build -t ecommerce-app .
-echo "Starting application..."
+echo Starting application...
 docker run -p 8080:8080 ecommerce-app
